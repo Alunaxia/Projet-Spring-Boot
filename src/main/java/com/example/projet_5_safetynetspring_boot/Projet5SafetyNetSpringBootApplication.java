ff4cac7f7028ab8f42a1632ca8bef341d3b6bc21
CAILLAREC Louis <louis.caillarec@amj-groupe.com>
2026-09-10 14:00:33 +0200

    Initialisation

---
+++ src/main/java/com/example/projet_5_safetynetspring_boot/Projet5SafetyNetSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_5_safetynetspring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet5SafetyNetSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projet5SafetyNetSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projet_5_safetynetspring_boot/Projet5SafetyNetSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_5_safetynetspring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet5SafetyNetSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
